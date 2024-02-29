56a4c00c968bf27073feef7e7be6a45b2cacefc9
Raphael Wirtz <raphael9w@gmail.com>
2024-02-29 23:03:35 +0100

    update: setup settings

--- setup/environment.sh
+++ setup/environment.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 # set manually via cli or in the shell config !
-export PICO_SDK_PATH="/home/rw/repos/3_resources/pico_sdk"
+export PICO_SDK_PATH="/home/rw/repos/3_resources/pico-sdk"
 export PICO_EXTRAS_PATH="/home/rw/repos/3_resources/pico-extras"
 export PICO_SEGGER_PATH="/home/rw/bin/jlink"
 export PICO_TOOLCHAIN_PATH="/usr/bin/arm-none-eabi-gcc-12.2"
+
+## fish shell
+# set -Ux PICO_SDK_PATH "/home/rw/repos/3_resources/pico-sdk"
+# set -Ux PICO_EXTRAS_PATH "/home/rw/repos/3_resources/pico-extras"
+# set -Ux PICO_SEGGER_PATH "/home/rw/bin/jlink"
+# set -Ux PICO_TOOLCHAIN_PATH "/usr/bin/arm-none-eabi-gcc-12.2"
