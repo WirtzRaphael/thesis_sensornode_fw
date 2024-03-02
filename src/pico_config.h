53765f6ab71270ee01bca721e84946d3b8aca7a2
Raphael Wirtz <raphael9w@gmail.com>
2024-03-02 19:01:45 +0100

    chg: configs for pico_w and hw v1.0

--- src/pico_config.h
+++ src/pico_config.h
@@ -3,6 +3,11 @@
 
 extern uint32_t SystemCoreClock;
 
+#define MODEL_PICO (0)
+#define MODEL_PICO_W (1)
+
+#define HW_SENSORNODE_V1 (1)
+
 /* UART
 */
 #define UART1_ID uart1
@@ -10,7 +15,12 @@ extern uint32_t SystemCoreClock;
 
 /* Pins
 */
-static const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+#if MODEL_PICO
+    static const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+#endif
+#if MODEL_PICO_W
+    //static const uint LED_PIN = CYW43_WL_GPIO_LED_PIN
+#endif
 #define PICO_PINS_I2C0_SDA 16
 #define PICO_PINS_I2C0_SCL 17
 #define PICO_PINS_UART1_TX 4
@@ -18,24 +28,29 @@ static const uint LED_PIN = PICO_DEFAULT_LED_PIN;
 
 /* Features
 */
-#define PICO_CONFIG_USE_TMP117 (1)
+#define PICO_CONFIG_USE_TMP117 (0)
 #define PICO_CONFIG_USE_SLEEP (0)
-#define PICO_CONFIG_USE_DISPLAY (1)
-#define PICO_CONFIG_USE_RADIO (1)
+#define PICO_CONFIG_USE_DISPLAY (0)
+#define PICO_CONFIG_USE_RADIO (0)
 #define PICO_CONFIG_USE_HEARTBEAT (1)
 
+/* PL : sensornode v1 - rp2040 pico w
+*/
+#if MODEL_PICO_W && HW_SENSORNODE_V1
+    #define PL_TEST_OUTPUT 29
+#endif
 
 /* PL : AEMBS Board
 */
-#define PL_CONFIG_USE_BUTTONS 0
-#define PL_SWITCH_MIDDLE 15
-#define PL_SWITCH_RIGHT 16
-#define PL_SWITCH_LEFT 17
-#define PL_SWITCH_DOWN 19
-#define PL_SWITCH_UP 20
+#define PL_AEMBS_CONFIG_USE_BUTTONS 0
+#define PL_AEMBS_SWITCH_MIDDLE 15
+#define PL_AEMBS_SWITCH_RIGHT 16
+#define PL_AEMBS_SWITCH_LEFT 17
+#define PL_AEMBS_SWITCH_DOWN 19
+#define PL_AEMBS_SWITCH_UP 20
 
-#define PL_LED_BLUE 18
-#define PL_LED_GREEN 19
-#define PL_LED_RED 20
+#define PL_AEMBS_LED_BLUE 18
+#define PL_AEMBS_LED_GREEN 19
+#define PL_AEMBS_LED_RED 20
 
 #endif /* PICO_CONFIG_H_ */
