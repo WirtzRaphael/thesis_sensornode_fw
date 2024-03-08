1b37615abbca4744367feb52067e27a26efcadfb
Raphael Wirtz <raphael9w@gmail.com>
2024-03-08 18:02:42 +0100

    add: shell to use mculog on rtt viewer

--- src/application.c
+++ src/application.c
@@ -81,6 +81,37 @@ static void AppTask(void *pv) {
   }
 }
 
+static uint8_t PrintStatus(McuShell_ConstStdIOType *io) {
+  unsigned char buf[48];
+
+  McuShell_SendStatusStr((unsigned char*)"app", (const unsigned char*)"Status of application\r\n", io->stdOut);
+  McuUtility_Num32uToStr(buf, sizeof(buf), PL_CONFIG_HW_ACTIVE_HW_VERSION/10);
+  McuUtility_chcat(buf, sizeof(buf), '.');
+  McuUtility_strcatNum8u(buf, sizeof(buf), PL_CONFIG_HW_ACTIVE_HW_VERSION%10);
+  McuUtility_strcat(buf, sizeof(buf), (unsigned char*)"\r\n");
+  McuShell_SendStatusStr((uint8_t*)"  HW", (unsigned char*)buf, io->stdOut);
+
+  McuUtility_strcpy(buf, sizeof(buf), (unsigned char*)APP_VERSION_STR);
+  McuUtility_strcat(buf, sizeof(buf), (unsigned char*)"\r\n");
+  McuShell_SendStatusStr((uint8_t*)"  version", buf, io->stdOut);
+
+  return ERR_OK;
+}
+
+uint8_t App_ParseCommand(const unsigned char *cmd, bool *handled, const McuShell_StdIOType *io) {
+  uint32_t value;
+  const unsigned char *p;
+
+  if (McuUtility_strcmp((char*)cmd, McuShell_CMD_HELP)==0 || McuUtility_strcmp((char*)cmd, "app help")==0) {
+    McuShell_SendHelpStr((unsigned char*)"app", (const unsigned char*)"Group of application commands\r\n", io->stdOut);
+    McuShell_SendHelpStr((unsigned char*)"  help|status", (const unsigned char*)"Print help or status information\r\n", io->stdOut);
+    *handled = true;
+    return ERR_OK;
+  } else if ((McuUtility_strcmp((char*)cmd, McuShell_CMD_STATUS)==0) || (McuUtility_strcmp((char*)cmd, "app status")==0)) {
+    *handled = true;
+    return PrintStatus(io);
+  }
+  return ERR_OK;
 }
 
 void APP_Run(void) {
