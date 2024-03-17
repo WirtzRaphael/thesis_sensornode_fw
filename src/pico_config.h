5d00454ff398ae768c493e74b2bbc2479ea2ad70
Raphael Wirtz <raphael9w@gmail.com>
2024-03-17 23:37:52 +0100

    format: code

--- src/pico_config.h
+++ src/pico_config.h
@@ -10,9 +10,8 @@ extern uint32_t SystemCoreClock;
 
 #define HW_PLATFORM (PL_CONFIG_HW_VERSION_1_0)
 
-
 /* UART
-*/
+ */
 #define UART0_ID uart0
 #define UART0_BAUD_RATE 19200
 
@@ -20,17 +19,16 @@ extern uint32_t SystemCoreClock;
 #define UART1_BAUD_RATE 19200
 
 /* Pins
-*/
+ */
 #if MODEL_PICO
-    static const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+static const uint LED_PIN = PICO_DEFAULT_LED_PIN;
 #endif
 #if MODEL_PICO_W
-    //static const uint LED_PIN = CYW43_WL_GPIO_LED_PIN
+  // static const uint LED_PIN = CYW43_WL_GPIO_LED_PIN
 #endif
 
-
 /* Features
-*/
+ */
 #define PICO_CONFIG_USE_TMP117 (1)
 #define PICO_CONFIG_USE_SLEEP (0)
 #define PICO_CONFIG_USE_DISPLAY (1)
@@ -38,36 +36,36 @@ extern uint32_t SystemCoreClock;
 #define PICO_CONFIG_USE_HEARTBEAT (1)
 
 /* PL : sensornode v1 - rp2040 pico w
-*/
+ */
 #if MODEL_PICO_W && HW_PLATFORM == PL_CONFIG_HW_VERSION_1_0
-    #define PICO_PINS_I2C0_SDA 8
-    #define PICO_PINS_I2C0_SCL 9
-    #define PICO_PINS_I2C1_SDA 6
-    #define PICO_PINS_I2C1_SCL 7
-    #define PICO_PINS_UART0_TX 16
-    #define PICO_PINS_UART0_RX 17
-    //#define PICO_PINS_UART1_TX 4
-    //#define PICO_PINS_UART1_RX 5
-    #define PL_GPIO_DISPLAY_ENABLE 3
-    #define PL_GPIO_ENABLE_VCC_RF 28
-    #define PL_GPIO_RADIO_RESET 26
-    #define PL_GPIO_TEST_OUTPUT 29
-    // todo UART, Radio
+  #define PICO_PINS_I2C0_SDA 8
+  #define PICO_PINS_I2C0_SCL 9
+  #define PICO_PINS_I2C1_SDA 6
+  #define PICO_PINS_I2C1_SCL 7
+  #define PICO_PINS_UART0_TX 16
+  #define PICO_PINS_UART0_RX 17
+  // #define PICO_PINS_UART1_TX 4
+  // #define PICO_PINS_UART1_RX 5
+  #define PL_GPIO_DISPLAY_ENABLE 3
+  #define PL_GPIO_ENABLE_VCC_RF 28
+  #define PL_GPIO_RADIO_RESET 26
+  #define PL_GPIO_TEST_OUTPUT 29
+  // todo UART, Radio
 #endif
 
 /* PL : AEMBS Board
-*/
+ */
 #if MODEL_PICO && HW_PLATFORM == PL_CONFIG_HW_AEMBS_BOARD
-    #define PL_AEMBS_CONFIG_USE_BUTTONS 0
-    #define PL_AEMBS_SWITCH_MIDDLE 15
-    #define PL_AEMBS_SWITCH_RIGHT 16
-    #define PL_AEMBS_SWITCH_LEFT 17
-    #define PL_AEMBS_SWITCH_DOWN 19
-    #define PL_AEMBS_SWITCH_UP 20
+  #define PL_AEMBS_CONFIG_USE_BUTTONS 0
+  #define PL_AEMBS_SWITCH_MIDDLE 15
+  #define PL_AEMBS_SWITCH_RIGHT 16
+  #define PL_AEMBS_SWITCH_LEFT 17
+  #define PL_AEMBS_SWITCH_DOWN 19
+  #define PL_AEMBS_SWITCH_UP 20
 
-    #define PL_AEMBS_LED_BLUE 18
-    #define PL_AEMBS_LED_GREEN 19
-    #define PL_AEMBS_LED_RED 20
+  #define PL_AEMBS_LED_BLUE 18
+  #define PL_AEMBS_LED_GREEN 19
+  #define PL_AEMBS_LED_RED 20
 #endif
 
 #endif /* PICO_CONFIG_H_ */
