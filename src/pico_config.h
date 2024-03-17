af8eb05e66f77a014b98994aafabb6b8e1f5e17c
Raphael Wirtz <raphael9w@gmail.com>
2024-03-18 00:01:29 +0100

    format: config pico

--- src/pico_config.h
+++ src/pico_config.h
@@ -5,17 +5,18 @@
 #include "stdint.h"
 extern uint32_t SystemCoreClock;
 
-#define MODEL_PICO (0)
+// todo add rp2040 only, rename file or move content
+#define MODEL_PICO   (0)
 #define MODEL_PICO_W (1)
 
 #define HW_PLATFORM (PL_CONFIG_HW_VERSION_1_0)
 
 /* UART
  */
-#define UART0_ID uart0
+#define UART0_ID        uart0
 #define UART0_BAUD_RATE 19200
 
-#define UART1_ID uart1
+#define UART1_ID        uart1
 #define UART1_BAUD_RATE 19200
 
 /* Pins
@@ -29,10 +30,10 @@ static const uint LED_PIN = PICO_DEFAULT_LED_PIN;
 
 /* Features
  */
-#define PICO_CONFIG_USE_TMP117 (1)
-#define PICO_CONFIG_USE_SLEEP (0)
-#define PICO_CONFIG_USE_DISPLAY (1)
-#define PICO_CONFIG_USE_RADIO (1)
+#define PICO_CONFIG_USE_TMP117    (1)
+#define PICO_CONFIG_USE_SLEEP     (0)
+#define PICO_CONFIG_USE_DISPLAY   (1)
+#define PICO_CONFIG_USE_RADIO     (1)
 #define PICO_CONFIG_USE_HEARTBEAT (1)
 
 /* PL : sensornode v1 - rp2040 pico w
@@ -47,9 +48,9 @@ static const uint LED_PIN = PICO_DEFAULT_LED_PIN;
   // #define PICO_PINS_UART1_TX 4
   // #define PICO_PINS_UART1_RX 5
   #define PL_GPIO_DISPLAY_ENABLE 3
-  #define PL_GPIO_ENABLE_VCC_RF 28
-  #define PL_GPIO_RADIO_RESET 26
-  #define PL_GPIO_TEST_OUTPUT 29
+  #define PL_GPIO_ENABLE_VCC_RF  28
+  #define PL_GPIO_RADIO_RESET    26
+  #define PL_GPIO_TEST_OUTPUT    29
   // todo UART, Radio
 #endif
 
@@ -57,15 +58,15 @@ static const uint LED_PIN = PICO_DEFAULT_LED_PIN;
  */
 #if MODEL_PICO && HW_PLATFORM == PL_CONFIG_HW_AEMBS_BOARD
   #define PL_AEMBS_CONFIG_USE_BUTTONS 0
-  #define PL_AEMBS_SWITCH_MIDDLE 15
-  #define PL_AEMBS_SWITCH_RIGHT 16
-  #define PL_AEMBS_SWITCH_LEFT 17
-  #define PL_AEMBS_SWITCH_DOWN 19
-  #define PL_AEMBS_SWITCH_UP 20
+  #define PL_AEMBS_SWITCH_MIDDLE      15
+  #define PL_AEMBS_SWITCH_RIGHT       16
+  #define PL_AEMBS_SWITCH_LEFT        17
+  #define PL_AEMBS_SWITCH_DOWN        19
+  #define PL_AEMBS_SWITCH_UP          20
 
-  #define PL_AEMBS_LED_BLUE 18
+  #define PL_AEMBS_LED_BLUE  18
   #define PL_AEMBS_LED_GREEN 19
-  #define PL_AEMBS_LED_RED 20
+  #define PL_AEMBS_LED_RED   20
 #endif
 
 #endif /* PICO_CONFIG_H_ */
