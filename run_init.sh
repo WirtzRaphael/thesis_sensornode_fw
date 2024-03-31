64eafbf96087bfdaad240fce748e6c99f7f2a04b
Raphael Wirtz <raphael9w@gmail.com>
2024-03-31 22:20:23 +0200

    add: compiler optimization flags

--- run_init.sh
+++ run_init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # - generate files with build flags for clangd
-cmake -G "Ninja" . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=1
+cmake -G "Ninja" . -B build -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=1
