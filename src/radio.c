0de86a2e65c3a84e8d0877d51f0bc145410b5136
Raphael Wirtz <raphael9w@gmail.com>
2024-05-10 17:16:16 +0200

    chg: radio function to send and receive for authentication

--- src/radio.c
+++ src/radio.c
@@ -18,14 +18,17 @@
 
 #include "pico/unique_id.h"
 #include <stdint.h>
+#include <stdio.h>
 #include <string.h>
 #include <sys/_types.h>
 
-#define RF_CHANNEL_DEFAULT (5)
+#define RF_CHANNEL_DEFAULT       (5)
 #define PROTOCOL_AUTH_SIZE_BYTES (10)
 
 // Use fix channel or scan channels
-#define FEAT_SCAN_CHANNELS (0)
+#define SCAN_CHANNELS_FOR_CONNECTION (0)
+#define TRANSMISSION_IN_BYTES        (0)
+#define ACTIVATE_RF                  (0)
 
 int channel_start = 0;
 int channel_end = 0;
@@ -46,14 +49,13 @@ void radio_init(void) {
 /**
  * @brief Scan radio network and authenticate.
  *
- * todo : fix channel or channel scan feature (time)
- * todo : use broadcast for tranmission (coverage vs security)
+ * todo : refactor when not direct radio buffer used
  */
 void radio_authentication(void) {
   rc232_uart_read_all(); // empty buffer
   McuLog_trace("radio : Scan network and authenticate\n");
 
-#if FEAT_SCAN_CHANNELS // scan channels
+#if SCAN_CHANNELS_FOR_CONNECTION
   channel_start = RC1701_RF_CHANNEL_MIN;
   channel_end = RC1701_RF_CHANNEL_MAX;
 #else // only default channel
@@ -74,35 +76,50 @@ void radio_authentication(void) {
 
     rc232_config_rf_channel_number(i);
     radio_send_authentication_request();
-    // todo : implement receive response and decode
-    //uint8_t buffer[PROTOCOL_AUTH_SIZE_BYTES] = {0};
-    uint8_t buffer[10];
-    for (int t = 0; t <= 300; t++) {
-      rc232_uart_read_byte(buffer);
-      if(buffer[0] == '0') {
-        // continue until protcol start character (A,A,C)
-        continue;
-      }
-      // todo : check for sequence 
-      McuLog_trace("radio : received %c\n", buffer[0]);
-      /*
-      if(radio_receive_authentication() == ERR_OK) {
-        // error code
-        McuLog_trace("radio : authentication successful\n");
-        break;
-      };
-      */
-      sleep_ms(10); // fixme : delay until sent
-    }
-    // todo wait response
-  }
+    /*
+     * Wait for response and read control character
+     * todo : refactor, don't read byte by byte
+     * todo : hscl lite protocol
+     */
+    // uint8_t buffer[PROTOCOL_AUTH_SIZE_BYTES] = {0};
+    uint8_t buffer1[1];
+    uint8_t buffer2[1];
+    error_t err = ERR_ARBITR;
+    for (int t = 0; t <= 500; t++) {
+      err = rc232_uart_read_byte(buffer1);
 
-  rc232_config_destination_address(20);
-  rc232_config_rf_channel_number(5);
-  // receive response
-  // - Free UID network (optional)
-  // - UID gateway -> DID
-  // - time sync (optional)
+      if (err == ERR_OK) {
+        // first char
+        McuLog_trace("radio : received %c\n", buffer1[0]);
+        printf("radio : received %c\n", buffer1[0]);
+        err = rc232_uart_read_byte(buffer2);
+        if (err == ERR_OK) {
+          // second char
+          printf("radio : received %c\n", buffer2[0]);
+          printf("radio: received %c %c\n", buffer1[0], buffer2[0]);
+        }
+      }
+      if (err == ERR_OK) {
+        sleep_ms(10); // fixme : delay until sent
+      }
+    }
+    /*
+     * Action based on received control character
+     */
+    if (buffer1[0] == 'A' && buffer2[0] == 'C') {
+      McuLog_trace("radio : acknowledge received\n");
+      // todo : action after acknowledge (payload values, set uid, channel)
+      break;
+    }
+  }
+}
+
+rc232_config_destination_address(20);
+rc232_config_rf_channel_number(5);
+// receive response
+// - Free UID network (optional)
+// - UID gateway -> DID
+// - time sync (optional)
 }
 
 void radio_send_authentication_request(void) {
@@ -111,36 +128,53 @@ void radio_send_authentication_request(void) {
   // todo : send -> tx (rc232)
   // -- frame delimiter flag
   // fixme : change buffer from char to int (binary)
-  char frame_flag[1] = { 0x7E};
-  rc232_send_string(frame_flag);
+  char frame_flag[5] = "0x7E";
   // -- payload
-  char message [3]; 
+  char payload[3];
   // todo : combine arrays and send all together
   // - address
   // NONE (before authentication not assigned)
   // - control field
   // authentication request
-  strcpy(message, "AR");
+  strcpy(payload, "AR");
+  printf("payload (string): %s\n", payload);
+  strcat(payload, "-");
+  printf("payload (string): %s\n", payload);
   // - board information
-  strcat(message, pico_uid_string);
-  // -- checksum 
-  printf("payload (string): %s\n", message);
-  // -- convert to binary
-  // todo : convert to binary
-  //unsigned char* message_byte = (unsigned char*) message;
-  //printf("payload (binary): %s\n", message_byte);
-  rc232_send_string(message);
-
-  // -- frame delimiter flag
-  //strcat(message, "0x7E"); // 
-  rc232_send_string(frame_flag);
+  printf("pico uid string: %s\n", pico_uid_string);
+  strcat(payload, pico_uid_string);
+  printf("payload (string): %s\n", payload);
+// -- checksum
+#if TRANSMISSION_IN_BYTES
+  McuLog_trace("convert message to binary");
+// -- convert to binary
+// todo : convert to binary and send binary
+// todo : bit stuffing
+// todo : escape character
+// todo : crc (optional)
+// unsigned char* message_byte = (unsigned char*) message;
+// printf("payload (binary): %s\n", message_byte);
+#else
+  // -- send
+  // todo : string stuffing
+  // todo : escape character
+  // todo : crc (optional)
+  // frame delimiter flag
+  if (ACTIVATE_RF) {
+    rc232_send_string(frame_flag);
+    // message
+    rc232_send_string(payload);
+    // frame delimiter flag
+    rc232_send_string(frame_flag);
+  }
+#endif
 }
 
 error_t radio_receive_authentication(void) {
   // todo : protocol authentication response
   // todo : receive -> rx (rc232)
   // receive via broadcast
-  //uint8_t buffer[PROTOCOL_AUTH_SIZE_BYTES] = {0};
+  // uint8_t buffer[PROTOCOL_AUTH_SIZE_BYTES] = {0};
   uint8_t buffer[10];
   if (rc232_uart_read_byte(buffer) != ERR_OK) {
     return ERR_FAILED;
