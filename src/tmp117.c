c236f165d12789de26813fda7209a13efaa4001a
Raphael Wirtz <raphael9w@gmail.com>
2024-05-11 18:46:58 +0200

    fix: tmp117 reactivate read id via i2c

--- src/tmp117.c
+++ src/tmp117.c
@@ -28,7 +28,7 @@ uint16_t tmp117_read_temperature(i2c_inst_t *i2c, const uint addr) {
 uint16_t tmp117_read_id(i2c_inst_t *i2c, const uint addr) {
     //static uint8_t buf[2];
     uint8_t buf[2];
-    //i2c_reg_read(i2c, addr, TMP117_DEVICE_ID, buf, 2);
+    i2c_reg_read(i2c, addr, TMP117_DEVICE_ID, buf, 2);
     uint16_t data16 = ((buf[0] << 8) | buf[1]); 
     return data16 & 0x0fff;
 }
