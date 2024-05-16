b987c80f2d5e09690a4a87cf9e8537e4f333de0d
Raphael Wirtz <raphael9w@gmail.com>
2024-05-16 15:28:01 +0200

    rm: remove cobs code

--- src/radio.h
+++ src/radio.h
@@ -1,7 +1,6 @@
 #ifndef RADIO_H_
 #define RADIO_H_
 
-#include "cobs.h"
 #include "sensors.h"
 #include <errno.h>
 #include <stdint.h>
@@ -14,22 +13,13 @@ static void log_hdlc_decoded(char *decoded_ptr, size_t decoded_len);
 static void log_buffer_as_char(char *buffer, size_t length);
 static void log_buffer_as_int(char *buffer, size_t length);
 void radio_authentication(void);
-void radio_encoding_cobs_example(void);
 void radio_encoding_hdlc_example(void);
 static void radio_send_authentication_request(void);
 static error_t radio_wait_for_authentication_response(uint32_t timeout_ms);
-/*
-cobs_encode_result radio_encode(void *encoded_payload_ptr,
-                                size_t encoded_payload_len,
-                                cobs_data *payload_bytes,
-                                size_t payload_bytes_len);
-                                */
 void radio_init(void);
 void radio_send_temperature_as_string(
     temperature_measurement_t *temperature_measurement, bool dryrun);
 void radio_send_temperature_as_bytes(
-    temperature_measurement_t *temperature_measurement, bool dryrun);
-void radio_send_temperature_as_bytes_cobs(
     temperature_measurement_t *temperature_measurement, bool dryrun);
 void radio_send_test(void);
 char radio_get_rf_destination_address(void);
