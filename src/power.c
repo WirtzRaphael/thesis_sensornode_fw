090da64e65f45db12f61211fe2baedd4cd5009af
Raphael Wirtz <raphael9w@gmail.com>
2024-05-23 10:11:04 +0200

    add: functions for power sources

--- src/power.c
+++ src/power.c
@@ -7,6 +7,7 @@
  * @copyright Copyright (c) 2024
  */
 
+#include "power.h"
 #include "McuLog.h"
 #include "pico/stdlib.h"
 #include "pico_config.h"
@@ -15,7 +16,6 @@
 
 #if PICO_CONFIG_USE_POWER
 
-// todo : move central
 void power_init(void) {
   /* Pin : 3V3 Power enable
    * note : HOLDS THE POWER SUPPLY
@@ -25,22 +25,62 @@ void power_init(void) {
   gpio_set_dir(PICO_PINS_LED_1, GPIO_OUT);
   // 3V3 : enable
   gpio_put(PICO_PINS_LED_1, true);
+  //power_3v3_1_enable(true);
 
   /* Pin : 3V3 Power mode
    * fix : use RS232 Pin
    */
   gpio_init(PICO_PINS_RS232_FORCEOFF_N);
   gpio_set_dir(PICO_PINS_RS232_FORCEOFF_N, GPIO_OUT);
-  // power mode : light
-  gpio_put(PICO_PINS_RS232_FORCEOFF_N, false); // power mode light
+  //power_mode(POWER_MODE_LIGHT);
 }
 
-// todo : function 3V3 power mode light/heavy
-// fix : use led
+/**
+ * @brief Enable or disable 3V3-1 power supply
+ * 
+ * @param enable true to enable, false to disable
+ *
+ * fix : use led pin
+ */
+void power_3v3_1_enable(bool enable) {
+  if (enable) {
+    gpio_put(PICO_PINS_LED_1, true);
+  } else {
+    gpio_put(PICO_PINS_LED_1, false);
+  }
+}
 
-// todo : 3V3-1 deactivate power
-// fix : use rs232 enable pin
+/**
+ * @brief Enable or disable 3V3-2 power supply
+ * 
+ * @param enable true to enable, false to disable
+ */
+void power_3v3_2_enable(bool enable) {
+  // Component not present
+  McuLog_error("Not implemented.");
+}
 
-// todo (cancelled) : 3V3-2 activate
+/**
+ * @brief Set power mode
+ * 
+ * @param mode power mode
+ * @return error_t 
+ *
+ * fix : use rs232 enable pin
+ */
+error_t power_mode(power_mode_t mode) {
+  switch (mode) {
+  case POWER_MODE_LIGHT:
+    gpio_put(PICO_PINS_RS232_FORCEOFF_N, false);
+    break;
+  case POWER_MODE_HEAVY:
+    gpio_put(PICO_PINS_RS232_FORCEOFF_N, true);
+    break;
+  default:
+    McuLog_error("Invalid power mode.");
+  }
+}
+
+// todo : rtc wakeup
 
 #endif /* PICO_CONFIG_USE_POWER */
--- src/power.h
+++ src/power.h
@@ -1,6 +1,17 @@
 #ifndef POWER_H_
 #define POWER_H_
 
+#include <errno.h>
+#include "pico/stdlib.h"
+
+typedef enum {
+  POWER_MODE_LIGHT,
+  POWER_MODE_HEAVY
+}power_mode_t;
+
 void power_init(void);
+void power_3v3_1_enable(bool enable);
+void power_3v3_2_enable(bool enable);
+error_t power_mode(power_mode_t mode);
 
 #endif /* POWER_H_ */
